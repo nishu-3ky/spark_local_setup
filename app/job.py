bfa57512d23c4f2a3ca6536b92184f6227722133
nishu-3ky <98794908+nishu-3ky@users.noreply.github.com>
2026-05-13 00:23:24 +0530

    working minimal changes

--- app/job.py
+++ app/job.py
@@ -4,17 +4,19 @@ import time
 
 spark = SparkSession.builder \
     .appName("MyJob") \
-    .master("spark://spark-master:7077") \
+    .master("local[*]") \
     .config("spark.executor.cores", "2") \
     .config("spark.cores.max", "4") \
     .config("spark.executor.memory", "2g") \
     .getOrCreate()
 
 print("\n\n=== Reading CSV ===\n")
-df = spark.read.csv("/opt/spark-data/input/data.csv", header=True, inferSchema=True)
+df = spark.read.csv("data/input/data.csv", header=True, inferSchema=True)
+df.show()
+df=df.filter(df.age>=28)
 df.show()
 
 print("\n\n=== Writing Output ===\n")
-df.write.mode("overwrite").csv("/opt/spark-data/output/result", header=True)
+df.write.mode("overwrite").csv("data/output/result", header=True)
 
 time.sleep(5000)
