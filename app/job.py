48544b3a4d47906a8e02c635049c61b070e99c77
nishu-3ky <98794908+nishu-3ky@users.noreply.github.com>
2026-05-12 23:53:27 +0530

    spark local working 1

--- app/job.py
+++ app/job.py
@@ -1,20 +1,41 @@
 from pyspark.sql import SparkSession
 import time
+import os
 
+os.environ["HADOOP_HOME"] = r"C:\Program Files\hadoop"
+os.environ["hadoop.home.dir"] = r"C:\Program Files\hadoop"
 
 spark = SparkSession.builder \
     .appName("MyJob") \
-    .master("spark://spark-master:7077") \
+    .master("local[*]") \
     .config("spark.executor.cores", "2") \
     .config("spark.cores.max", "4") \
-    .config("spark.executor.memory", "2g") \
+    .config("spark.executor.memory", "1g") \
+    .config("spark.sql.shuffle.partitions", "4") \
+    .config("spark.hadoop.fs.file.impl", "org.apache.hadoop.fs.RawLocalFileSystem") \
+    .config("spark.hadoop.fs.AbstractFileSystem.file.impl", "org.apache.hadoop.fs.local.LocalFs")\
+    .config("spark.hadoop.io.native.lib.available", "false") \
     .getOrCreate()
 
 print("\n\n=== Reading CSV ===\n")
-df = spark.read.csv("/opt/spark-data/input/data.csv", header=True, inferSchema=True)
+
+df = spark.read.csv(
+    "data/input/data.csv",
+    header=True,
+    inferSchema=True
+)
+
 df.show()
 
 print("\n\n=== Writing Output ===\n")
-df.write.mode("overwrite").csv("/opt/spark-data/output/result", header=True)
 
-time.sleep(5000)
+output_path = "file:///D:/prep/DataEngineer/PySpark/SetupCode/spark_local_setup/data/output/result"
+
+df.write \
+    .mode("overwrite") \
+    .option("header", True) \
+    .csv(output_path)
+
+print("Write Successful")
+
+spark.stop()
